73218668ebce9388c22eba5b695c4e90f979d4d6
Michael Ball <miguelpelota1@yahoo.com>
2015-05-28 14:31:31 -0700

    PINTEREST :: Add rich pin Open Graph meta tags to header on product pages

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>n50 | <?php echo $this->page ?></title>
+
+    <?php if ( get_class($this) == 'Product' ) : ?>
+        <meta property="og:title" content="<?php echo $this->product['name'] ?>" />
+        <meta property="og:description" content="<?php echo $this->product['description'] ?>" />
+        <meta property="og:type" content="product" />
+        <meta property="og:url" content="http://<?php echo $_SERVER['SERVER_NAME'] ?>/product/<?php echo $this->product['slug'] ?>" />
+        <meta property="og:image" content="http://<?php echo $_SERVER['SERVER_NAME'] ?><?php echo $this->product['images'][0] ?>" />
+        <meta property="og:site_name" content="Central Dozen" />
+        <meta property="og:price:amount" content="<?php echo number_format($this->product['price'], 2) ?>" />
+        <meta property="og:price:currency" content="USD" />
+        <meta property="og:availability" content="instock" />
+        <?php foreach ( $this->product['related_products'] as $related_product ) : ?>
+            <meta property="og:see_also" content="http://<?php echo $_SERVER['SERVER_NAME'] ?>/product/<?php echo $related_product['slug'] ?>" />
+        <?php endforeach; ?>
+    <?php endif; ?>
+
+
     <link rel="stylesheet" type="text/css" href="/skin/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/skin/css/styles.css">
+
 </head>
 <body>
 
